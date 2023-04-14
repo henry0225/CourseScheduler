2eaea6a2e3f3ef39fccd2b27ce441e5246f4abd1
BIGBOYHENRY <55297309+BIGBOYHENRY@users.noreply.github.com>
2023-04-14 17:42:17 -0500

    Connected API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (event) => {
+    setNum1(event.target.value);
+  }
+
+  const handleNum2Change = (event) => {
+    setNum2(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { num1, num2 };
+    fetch('http://127.0.0.1:6969/test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => setResult(data.result))
+      .catch(error => console.error(error))
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form onSubmit={handleSubmit} style={{ display: 'block' }}>
+        <label style={{ display: 'block' }}>
+          Number 1:
+          <input type="number" value={num1} onChange={handleNum1Change} />
+        </label>
+        <label style={{ display: 'block' }}>
+          Number 2:
+          <input type="number" value={num2} onChange={handleNum2Change} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {result && <p>Result: {result}</p>}
     </div>
   )
 }
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import requests
 import json
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, jsonify
 from flask_restful import Resource, Api, reqparse
 from file_parser import openFile
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app)
 
-@app.route('/ping')
+@app.route('/')
 def ping():
     return 'get pinged'
 
-@app.route('/signUpUser', methods=['POST'])
-def get_courses():
-    user =  request.form['username'];
-    password = request.form['password'];
-    return json.dumps({'status':'OK','user':user,'pass':password});
+@app.route('/test', methods = ['GET', 'POST', 'OPTIONS'])
+def test():
+    if request.method == 'POST':
+        data = request.get_json()
+        num1 = int(data['num1'])
+        num2 = int(data['num2'])
+        result = num1 + num2
+        response = {'status': 'OK', 'result': result}
+        return jsonify(response)
+    else:
+        return jsonify({'status': 'Error', 'message': 'Invalid request method'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=6969)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
