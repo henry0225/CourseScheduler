e8f2e713c2dd3af24273fce06761cbf13af18477
lgang2 <34961729+lgang2@users.noreply.github.com>
2023-04-15 15:59:39 -0500

    Reverted back to just main function for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,29 +5,29 @@ import './App.css'
 
 function App() {
   const [num1, setNum1] = useState('');
-  const [num2, setNum2] = useState('');
-  const [num3, setNum3] = useState('');
-  const [num4, setNum4] = useState('');
+  // const [num2, setNum2] = useState('');
+  // const [num3, setNum3] = useState('');
+  // const [num4, setNum4] = useState('');
   const [result, setResult] = useState('');
 
   const handleNum1Change = (event) => {
     setNum1(event.target.value);
   }
 
-  const handleNum2Change = (event) => {
-    setNum2(event.target.value);
-  }
-  const handleNum3Change = (event) => {
-    setNum3(event.target.value);
-  }
-  const handleNum4Change = (event) => {
-    setNum4(event.target.value);
-  }
+  // const handleNum2Change = (event) => {
+  //   setNum2(event.target.value);
+  // }
+  // const handleNum3Change = (event) => {
+  //   setNum3(event.target.value);
+  // }
+  // const handleNum4Change = (event) => {
+  //   setNum4(event.target.value);
+  // }
 
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = { num1, num2, num3, num4 };
+    const data = { num1 };
     fetch('http://127.0.0.1:6969/test', {
       method: 'POST',
       headers: {
@@ -47,7 +47,7 @@ function App() {
           Courses (ex: COMP 140,COMP 182,COMP 215):
           <input type="text" value={num1} onChange={handleNum1Change} />
         </label>
-        <label style={{ display: 'block' }}>
+        {/* <label style={{ display: 'block' }}>
           Custom Time Blocks Name:
           <input type="text" value={num2} onChange={handleNum2Change} />
         </label>
@@ -58,7 +58,7 @@ function App() {
         <label style={{ display: 'block' }}>
           Custom Time Block End Time:
           <input type="text" value={num4} onChange={handleNum4Change} />
-        </label>
+        </label> */}
         <button type="submit">Submit</button>
       </form>
       {result && (
--- main.py
+++ main.py
@@ -18,17 +18,17 @@ def test():
     if request.method == 'POST':
         data = request.get_json()
         course_info = str(data['num1'])
-        additional_course_keys = str(data['num2'])
-        additional_course_start_times = str(data['num3'])
-        additional_course_end_times = str(data['num4'])
-        additional_course_keys = additional_course_keys.split(',')
-        additional_course_start_times = additional_course_start_times.split(',')
-        additional_course_end_times = additional_course_end_times.split(',')
-        additional_courses = defaultdict(list)
-        for i in range(len(additional_course_keys)):
-            additional_courses[additional_course_keys[i]].append([additional_course_start_times[i], additional_course_end_times[i]])
+        # additional_course_keys = str(data['num2'])
+        # additional_course_start_times = str(data['num3'])
+        # additional_course_end_times = str(data['num4'])
+        # additional_course_keys = additional_course_keys.split(',')
+        # additional_course_start_times = additional_course_start_times.split(',')
+        # additional_course_end_times = additional_course_end_times.split(',')
+        # additional_courses = defaultdict(list)
+        # for i in range(len(additional_course_keys)):
+        #     additional_courses[additional_course_keys[i]].append([additional_course_start_times[i], additional_course_end_times[i]])
         courses = course_info.split(',')
-        courses_list = algo(courses, additional_courses)
+        courses_list = algo(courses, additional_courses={})
         result = courses_list
 
         response = {'status': 'OK', 'result': result}
