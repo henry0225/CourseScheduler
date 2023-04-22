c85ddc4d2c3f75d8effeb65b1632b22a0d8f7df0
lgang2 <34961729+lgang2@users.noreply.github.com>
2023-04-22 00:37:06 -0500

    Added colors and some minor ui improvements

--- frontend/components/calendar.jsx
+++ frontend/components/calendar.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
 import "./CalendarStyles.css";
+
 const styles = {
   wrap: {
     display: "flex"
@@ -14,61 +15,101 @@ const styles = {
 };
 
 class Calendar extends Component {
-    constructor(props) {
-      super(props);
-      this.calendarRef = React.createRef();
-      this.state = {
-        viewType: "Week",
-        durationBarVisible: true,
-        timeRangeSelectedHandling: "Enabled",
-        onTimeRangeSelected: async args => {
-          const dp = this.calendar;
-          const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
-          dp.clearSelection();
-          if (!modal.result) { return; }
-          dp.events.add({
-            start: args.start,
-            end: args.end,
-            id: DayPilot.guid(),
-            text: modal.result
-          });
-        },
-        eventDeleteHandling: "Update",
-        onEventClick: async args => {
-          const dp = this.calendar;
-          const modal = await DayPilot.Modal.prompt("Update event text:", args.e.text());
-          if (!modal.result) { return; }
-          const e = args.e;
-          e.data.text = modal.result;
-          dp.events.update(e);
-        },
-        headerDateFormat: "dddd", // set the column header to display day names
-      };
-    }
-  
-    get calendar() {
-      return this.calendarRef.current.control;
-    }
-  
-    componentDidMount() {
-      console.log("kekee", this.props.events)
-      const { startDate } = this.props;
-      const events = this.props.events;
-  
-      this.calendar.update({startDate, events});
-    }
-  
-    render() {
-      return (
-        <div style={styles.main}>
-          <DayPilotCalendar
-            {...this.state}
-            ref={this.calendarRef}
-          />
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.calendarRef = React.createRef();
+    this.state = {
+      eventMoveHandling: "Update",
+      eventResizeHandling: "Update",
+      viewType: "Week",
+      durationBarVisible: true,
+      timeRangeSelectedHandling: "Enabled",
+      onTimeRangeSelected: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+        dp.clearSelection();
+        if (!modal.result) { return; }
+        dp.events.add({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          text: modal.result
+        });
+      },
+      eventDeleteHandling: "Update",
+      onEventClick: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt("Update event text:", args.e.text());
+        if (!modal.result) { return; }
+        const e = args.e;
+        e.data.text = modal.result;
+        dp.events.update(e);
+      },
+      headerDateFormat: "dddd", // set the column header to display day names
+      onBeforeEventRender: args => {
+        const color = this.getColorForEvent(args.data.text);
+        args.data.backColor = color;
+        args.data.borderRadius = "5px"; // Set the border radius to 5 pixels
+      }
+    };
   }
+
+  get calendar() {
+    return this.calendarRef.current.control;
+  }
+
+  componentDidMount() {
+    const { startDate } = this.props;
+    const events = this.props.events;
+
+    this.calendar.update({startDate, events});
+  }
+
+  getColorForEvent(text) {
+    // Define an array of allowed hue values
+    const hues = [100, 120, 140, 160, 180, 200, 220, 240];
   
+    // Generate a hash value based on the text attribute
+    const hash = text.split('').reduce((acc, val) => {
+      acc = ((acc << 5) - acc) + val.charCodeAt(0);
+      return acc & acc;
+    }, 0);
+  
+    // Use the hash to select a hue from the allowed hues array
+    const hue = hues[Math.abs(hash) % hues.length];
+  
+    // Set the saturation and lightness values to fixed values
+    const saturation = 70;
+    const lightness = 80;
+  
+    // Construct and return the HSL color string
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+
+  render() {
+    const calendarStyle = {
+      borderRadius: "20px",
+      overflow: "hidden", // ensure that content is clipped at the rounded corners
+      
+    };
+    const eventStyle = {
+      borderRadius: "20px",
+      overflow: "hidden", // ensure that content is clipped at the rounded corners
+      border: "5px",
+      fontSize: "10pt",
+      padding: "5px",
+      opacity: "0.8",
+    };
+    return (
+      <div style={calendarStyle}>
+        <DayPilotCalendar
+          {...this.state}
+          ref={this.calendarRef}
+          eventStyle={eventStyle}
+        />
+      </div>
+    );
+  }
+}
 
 export default Calendar;
